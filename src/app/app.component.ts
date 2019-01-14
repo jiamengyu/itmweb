760aeb61677428b864a95d8a44e70a922bfa4b08
Yu <Jiameng.Yu@td.com>
2019-01-14 15:36:05 -0500

    add sass support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'itm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'itmweb';
+  title = 'ITMercenaries Portal';
 }
