f82f0299f07e3780445f85eb007df0f91bfc8524
Yu <Jiameng.Yu@td.com>
2019-01-21 13:59:29 -0500

    Update folder structure

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -3,5 +3,3 @@ export * from './component/buttons';
 export * from './list-errors.component';
 export * from './shared.module';
 export * from './directive/show-authed.directive';
-export * from './component/footer.component';
-export * from './component/header.component';
