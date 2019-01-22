06c49c0ea318de43220e4d7604ef928668c34d47
Yu <Jiameng.Yu@td.com>
2019-01-22 15:28:30 -0500

    built-in directives

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public myVar: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.myVar ='C';
   }
 
 }
