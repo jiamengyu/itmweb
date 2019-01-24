5605e7eb1acf6c652ca3da525730e6fb4778f1c2
Yu <Jiameng.Yu@td.com>
2019-01-24 16:38:16 -0500

    Add sticky header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'itm-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('void', style({opacity: 0})),
+      transition(':enter', [ animate(300)]),
+      transition(':leave', [ animate(500)])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   public myVar: string;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document) { }
 
   ngOnInit() {
     this.myVar = 'C';
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e){
+
+    let element = document.getElementById('navbar');
+    if (window.pageYOffset >30){
+      element.classList.add('sticky');
+    } else {
+      element.classList.remove('sticky');
+    }
+  }
 }
